eec72d69bff76a55e16725699cb89aee73cc4e99
aleeex <akang2@gmail.com>
2016-08-13 16:18:58 -0700

    understand a lot more, need to practice a few more times to solidify

--- controller.js
+++ controller.js
@@ -1,5 +1,17 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular
+  .module('userProfiles')
+  .controller('MainController', function(mainService, $scope){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+    $scope.percent = "32%  + 62%";
+
+    $scope.getUsers = function() {
+      $scope.users = mainService.getUsers();
+    }
+
+    $scope.getUsers();
+
+    $scope.toggleFavorite = mainService.toggleFavorite;
+
 })
---
+++ mainService.js
@@ -0,0 +1,35 @@
+angular
+  .module('userProfiles')
+  .service('mainService', function() {
+
+    var data = [{
+        "id": 1,
+        "first_name": "george",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg",
+        "isFavorite": false
+    },
+    {
+        "id": 2,
+        "first_name": "lucille",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg",
+        "isFavorite": false
+    },
+    {
+        "id": 3,
+        "first_name": "oscar",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg",
+        "isFavorite": false
+    }];
+
+    this.getUsers = function() {
+      return data;
+    }
+
+    this.toggleFavorite = function(index) {
+      data[index].isFavorite = !data[index].isFavorite;
+    }
+
+  });
