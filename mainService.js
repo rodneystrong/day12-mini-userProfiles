7ec63e8ad728eab8750318600c884b874538b761
aleeex <akang2@gmail.com>
2016-08-13 16:30:00 -0700

    changing from  in html to user.id with changes to id value to test if it works

--- mainService.js
+++ mainService.js
@@ -3,21 +3,21 @@ angular
   .service('mainService', function() {
 
     var data = [{
-        "id": 1,
+        "id": 0,
         "first_name": "george",
         "last_name": "bluth",
         "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg",
         "isFavorite": false
     },
     {
-        "id": 2,
+        "id": 1,
         "first_name": "lucille",
         "last_name": "bluth",
         "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg",
         "isFavorite": false
     },
     {
-        "id": 3,
+        "id": 2,
         "first_name": "oscar",
         "last_name": "bluth",
         "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg",
