33318e7c1fc9277ab3d192421c9211e8e28bf569
aleeex <akang2@gmail.com>
2016-08-20 03:36:34 -0600

    much better now. the parameter in the toggleFavorite function doesn't matter.  knows what to do

--- controller.js
+++ controller.js
@@ -1,5 +1,15 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular
+  .module('userProfiles')
+  .controller('MainController', function($scope, mainService){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+    $scope.getData = function() {
+      $scope.users = mainService.getData();
+    }
+
+    $scope.getData();
+
+    $scope.toggleFavorite = mainService.toggleFavorite;
+
 })
