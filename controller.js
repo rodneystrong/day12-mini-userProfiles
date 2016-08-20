9feaae532d4c56dcbe0d0a4f0895c92075072e10
aleeex <akang2@gmail.com>
2016-08-20 02:53:12 -0600

    definitely need more practice with understanding function creation on service and then injecting into controller

--- controller.js
+++ controller.js
@@ -1,5 +1,16 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular
+  .module('userProfiles')
+  .controller('MainController', function($scope, mainService, $http){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+    $scope.getUsers = function() {
+      $scope.users = mainService.getUsers();
+    }
+
+    $scope.toggleFavorite = mainService.toggleFavorite;
+
+    $scope.getUsers();
+
+
 })
